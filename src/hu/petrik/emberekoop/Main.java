619dd664db883c8812fd232a58ff018c2467b634
Maurisz02 <p2002adam@gmail.com>
2022-09-20 15:02:34 +0200

    alap feladatok elvegezve

--- src/hu/petrik/emberekoop/Main.java
+++ src/hu/petrik/emberekoop/Main.java
@@ -4,9 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Ember e1 = new Ember("Adam", "2002-11-29","Telki");
         Ember e2 = new Ember("Pinter", "2002-10-29","Telki");
-        Ember e3 = new Ember("Pintyo", "2002-09-29","Telki");
-        System.out.println(e1);
-        System.out.println(e2);
-        System.out.println(e3);
+        Ember e3 = new Ember("Pintyo", "2002-09-28","Telki");
+        Ember e4 = new Ember("Valaki", "2002-01-25","Telki");
+        Ember e5 = new Ember("Jani", "2002-04-24","Telki");
+
+        Emberek emberek = new Emberek(new Ember[]{e1, e2, e3, e4, e5});
+
+        System.out.println(emberek);
     }
 }
