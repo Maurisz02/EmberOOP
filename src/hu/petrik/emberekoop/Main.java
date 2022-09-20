9f5d7f48c21241db83de2807ecb25fcc34d280a0
Maurisz02 <p2002adam@gmail.com>
2022-09-20 14:08:19 +0200

    string formazas plusz 3 ember

--- src/hu/petrik/emberekoop/Main.java
+++ src/hu/petrik/emberekoop/Main.java
@@ -3,6 +3,10 @@ package hu.petrik.emberekoop;
 public class Main {
     public static void main(String[] args) {
         Ember e1 = new Ember("Adam", "2002-11-29","Telki");
+        Ember e2 = new Ember("Pinter", "2002-10-29","Telki");
+        Ember e3 = new Ember("Pintyo", "2002-09-29","Telki");
         System.out.println(e1);
+        System.out.println(e2);
+        System.out.println(e3);
     }
 }
