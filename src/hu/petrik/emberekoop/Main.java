f28839e66bfdcc41b22b459dc876d107d77a8fb9
Maurisz02 <p2002adam@gmail.com>
2022-09-20 15:27:30 +0200

    orai kesz

--- src/hu/petrik/emberekoop/Main.java
+++ src/hu/petrik/emberekoop/Main.java
@@ -1,7 +1,10 @@
 package hu.petrik.emberekoop;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         Ember e1 = new Ember("Adam", "2002-11-29","Telki");
         Ember e2 = new Ember("Pinter", "2002-10-29","Telki");
         Ember e3 = new Ember("Pintyo", "2002-09-28","Telki");
@@ -10,6 +13,16 @@ public class Main {
 
         Emberek emberek = new Emberek(new Ember[]{e1, e2, e3, e4, e5});
 
-        System.out.println(emberek);
+        //System.out.println(emberek);
+
+        try{
+            Emberek emberek2 = new Emberek("emberek.txt");
+            System.out.println(emberek2);
+        }catch (FileNotFoundException e){
+            System.out.println("Hiba az emberek.txt fájl nem található");
+        }catch (IOException e){
+            System.err.println("Ismeretlen hiba történt a fájl beolvasás folyamán");
+        }
+
     }
 }
