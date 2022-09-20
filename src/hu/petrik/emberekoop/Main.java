5c304a89c8b7489679c1f046005aba0cd40daec9
Maurisz02 <p2002adam@gmail.com>
2022-09-20 13:57:18 +0200

    alap oop struktura

--- src/hu/petrik/emberekoop/Main.java
+++ src/hu/petrik/emberekoop/Main.java
@@ -2,6 +2,7 @@ package hu.petrik.emberekoop;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Ember e1 = new Ember("Adam", "2002-11-29","Telki");
+        System.out.println(e1);
     }
 }
